ce7c76883b090708ab3264d8cd570da84af0ea3a
KarlosViBu <carlosurielvillegas@gmail.com>
2023-08-15 10:23:29 -0400

    Primer commit

---
+++ pages/index.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-container>
+
+  <ApolloQuery :query="query">
+    <template slot-scope="{result:{ data }}">
+      {{ data }}
+      <!-- <div v-for="categoria in data.categories" :key="categoria.id">
+        <v-btn
+          :to="{
+            name: 'category',
+            params: { category: categoria.slug },
+          }"
+          class="my-1"
+          >{{ categoria.name }}</v-btn
+        >
+      </div> -->
+    </template>
+  </ApolloQuery>
+</v-container>
+</template>
+
+<script>
+import gql from "graphql-tag";
+export default {
+  data() {
+    return {
+      query: gql`
+        query {
+          categories {
+            data {
+              id
+              attributes {
+                name
+                icon
+                slug
+                img
+              }
+            }
+          }
+        }
+      `,
+    };
+  },
+};
+</script>
